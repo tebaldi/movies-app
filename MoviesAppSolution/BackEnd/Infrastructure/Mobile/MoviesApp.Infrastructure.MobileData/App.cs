b1932b9bd6459d004c631387fc5947c1965cca71
Tebaldi <fabiotebaldi@gmail.com>
2016-11-05 00:53:10 -0200

    connection tester

--- MoviesAppSolution/BackEnd/Infrastructure/Mobile/MoviesApp.Infrastructure.MobileData/App.cs
+++ MoviesAppSolution/BackEnd/Infrastructure/Mobile/MoviesApp.Infrastructure.MobileData/App.cs
@@ -12,21 +12,14 @@ using Android.Widget;
 using Autofac;
 using MoviesApp.Infrastructure.MobileData.DependencyInjection;
 using MoviesApp.Services.ServiceFactory;
+using System.IO;
 
 namespace MoviesApp.Infrastructure.MobileData
 {
     public class App : Application
     {
-        static App()
-        {
-            
-        }
-
-        public static T ResolveFactory<T>() where T : IServiceFactory
-        {
-            var factory = DependencyResolver.Resolve<T>();
-            return factory;
-        }
+        public static string AppPath =
+            Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "MoviesApp";
 
         protected App(IntPtr javaReference, JniHandleOwnership transfer) 
             : base(javaReference, transfer)
@@ -36,6 +29,18 @@ namespace MoviesApp.Infrastructure.MobileData
         public override void OnCreate()
         {
             base.OnCreate();
+        }
+
+        private void ConfigureAppDirectory()
+        {
+            if (!Directory.Exists(AppPath))
+                Directory.CreateDirectory(AppPath);
+        }
+
+        public static T ResolveFactory<T>() where T : IServiceFactory
+        {
+            var factory = DependencyResolver.Resolve<T>();
+            return factory;
         }
     }
 }
